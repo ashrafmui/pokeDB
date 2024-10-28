9d7f68250a00307c7d856390f894223731b1349e
ashrafmui <ashrafmuhaiminul253@gmail.com>
2024-10-28 00:35:08 -0400

    fixed active state generation button issue on main page

--- pokedb/src/components/GenerationButtons.tsx
+++ pokedb/src/components/GenerationButtons.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 
 interface GneerationButtonsProps {
-  activeGeneration: number;
+  activeGeneration: number | null;
 }
 
 export default function GenerationButtons({ activeGeneration }: GneerationButtonsProps) {
